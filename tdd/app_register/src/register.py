0a36068ff426037e2d51a1a92f78a010970511bc
Guillaume Pealat <guillaume.pealat@gallium-ip.com>
2024-11-20 13:09:05 +0000

    Used flake8 on register.py to clear the code

--- tdd/app_register/src/register.py
+++ tdd/app_register/src/register.py
@@ -1,12 +1,14 @@
 import re
 
-def register(username: str, email: str, password:str)->bool:
+
+def register(username: str, email: str, password: str) -> bool:
     """Function that tests the validity of the username, email and password
 
     Args:
         username (str): It must not have empty spaces or being empty
         email (str): It must contain @ and .
-        password (str): It must be 8 character long, at least 1 number, at least 1 letter and at least 1 special character
+        password (str): It must be 8 character long, at least 1 number,
+        at least 1 letter and at least 1 special character
 
     Returns:
         bool: Returns True if all the checks are passed, False otherwise
@@ -28,20 +30,21 @@ def register(username: str, email: str, password:str)->bool:
         check_email = False
 
     # Checking the password
-    if len(password)<8:
+    if len(password) < 8:
         check_password = False
 
     # Checking that we have the required complexity
     pattern = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$"
 
-
     if check_password and not re.match(pattern, password):
         check_password = False
 
-    # The output is simply the multiplication of 3 booleans: if one is false, it is all false
+    # The output is simply the multiplication of 3 booleans:
+    # if one is false, it is all false
     return check_username * check_email * check_password
 
-def get_email_from_input()->str:
+
+def get_email_from_input() -> str:
     """Function that get the email from an input and validate it
 
     Returns:
@@ -53,8 +56,9 @@ def get_email_from_input()->str:
         print('Email invalid')
     else:
         return email
-    
-def get_username_from_input()->str:
+
+
+def get_username_from_input() -> str:
     """Function that get the user input for the username and validates it
 
     Returns:
@@ -67,7 +71,8 @@ def get_username_from_input()->str:
         print('Username invalid')
     else:
         return username
-    
+
+
 def get_password_from_input():
     """Function that takes an input for password and validates it
 
@@ -76,8 +81,8 @@ def get_password_from_input():
     """
     password = input("What is your password?")
 
-     # Checking the password
-    if len(password)<8:
+    # Checking the password
+    if len(password) < 8:
         print('Wrong password')
         return None
 
