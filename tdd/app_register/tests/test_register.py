f9e730813c639e585f0e2d961c9a0ee51259dbfb
Guillaume Pealat <guillaume.pealat@gallium-ip.com>
2024-11-20 14:17:04 +0000

    trying failing workflow

--- tdd/app_register/tests/test_register.py
+++ tdd/app_register/tests/test_register.py
@@ -1,6 +1,6 @@
 """Test module"""
-import pytest
 import io
+import pytest
 
 from app_register.src import register
 
@@ -9,29 +9,29 @@ def test_username():
     """Test function for the username
     """
     # Testing the empty case
-    assert register.register('','toto@gmail.com','1234@aze') == False
+    assert register.register('','toto@gmail.com','1234@aze') is False
     # Testing the space case
-    assert register.register('rr rr','toto@gmail.com','1234@aze') == False
+    assert register.register('rr rr','toto@gmail.com','1234@aze') is False
 
 def test_email():   
     """Test function for the email
     """
     # Testing the email without @
-    assert register.register('username','totogmail.com','1234@aze') == False
+    assert register.register('username','totogmail.com','1234@aze') is False
     # Testing the email without .
-    assert register.register('username','toto@gmailcom','1234@aze') == False
+    assert register.register('username','toto@gmailcom','1234@aze') is False
 
 def test_password():
     """Test function for the password
     """
     # Testing the case with less than 8 characters
-    assert register.register('username','toto@gmail.com','1234@az') == False
+    assert register.register('username','toto@gmail.com','1234@az') is False
     # TEsting the case with missing digits
-    assert register.register('username','toto@gmail.com','abcd@azd') == False
+    assert register.register('username','toto@gmail.com','abcd@azd') is False
     # Testing the case with missing characters
-    assert register.register('username','toto@gmail.com','1234@123') == False
+    assert register.register('username','toto@gmail.com','1234@123') is False
     # TEsting the case with missing special character
-    assert register.register('username','toto@gmail.com','12341123') == False
+    assert register.register('username','toto@gmail.com','12341123') is True
 
 #### Using Monkeypatch
 
