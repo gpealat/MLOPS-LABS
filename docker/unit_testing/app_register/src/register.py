b7e05ec2716fef0bd1e6750c7bccec6978060af0
Guillaume Pealat <guillaume.pealat@gallium-ip.com>
2024-11-21 13:47:47 +0000

    Added the docker lab

---
+++ docker/unit_testing/app_register/src/register.py
@@ -0,0 +1,97 @@
+"""Module to check the email, username and password"""
+import re
+
+
+def register(username: str, email: str, password: str) -> bool:
+    """Function that tests the validity of the username, email and password
+
+    Args:
+        username (str): It must not have empty spaces or being empty
+        email (str): It must contain @ and .
+        password (str): It must be 8 character long, at least 1 number,
+        at least 1 letter and at least 1 special character
+
+    Returns:
+        bool: Returns True if all the checks are passed, False otherwise
+    """
+    # Setting the default case
+    check_username = True
+    check_email = True
+    check_password = True
+
+    # Checking the username
+    if (username == '') or (' ' in username):
+        check_username = False
+
+    # Checking the email
+    # Regex to check both @ and .
+    pattern = r"@.*\."
+
+    if not re.search(pattern, email):
+        check_email = False
+
+    # Checking the password
+    if len(password) < 8:
+        check_password = False
+
+    # Checking that we have the required complexity
+    pattern = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$"
+
+    if check_password and not re.match(pattern, password):
+        check_password = False
+
+    # The output is simply the multiplication of 3 booleans :
+    # if one is false, it is all false
+    return check_username * check_email * check_password
+
+
+def get_email_from_input() -> str:
+    """Function that get the email from an input and validate it
+
+    Returns:
+        str: returns the email if everything is fine
+    """
+    email = input("Tell me your email:")
+
+    if ('@' not in email or "." not in email):
+        print('Email invalid')
+    else:
+        return email
+
+
+def get_username_from_input() -> str:
+    """Function that get the user input for the username and validates it
+
+    Returns:
+        str: username if valid
+    """
+    username = input('Tell me your username:')
+
+    # Checking the username
+    if (username == '') or (' ' in username):
+        print('Username invalid')
+    else:
+        return username
+
+
+def get_password_from_input():
+    """Function that takes an input for password and validates it
+
+    Returns:
+        _type_: validated password
+    """
+    password = input("What is your password?")
+
+    # Checking the password
+    if len(password) < 8:
+        print('Wrong password')
+        return None
+
+    # Checking that we have the required complexity
+    pattern = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$"
+
+    if not re.match(pattern, password):
+        print('Wrong password')
+        return None
+    else:
+        return password
---
+++ docker/unit_testing/app_register/tests/test_register.py
@@ -0,0 +1,154 @@
+"""Test module"""
+import io
+import pytest
+
+from app_register.src import register
+
+
+def test_username():
+    """Test function for the username
+    """
+    # Testing the empty case
+    assert register.register('','toto@gmail.com','1234@aze') is False
+    # Testing the space case
+    assert register.register('rr rr','toto@gmail.com','1234@aze') is False
+
+def test_email():   
+    """Test function for the email
+    """
+    # Testing the email without @
+    assert register.register('username','totogmail.com','1234@aze') is False
+    # Testing the email without .
+    assert register.register('username','toto@gmailcom','1234@aze') is False
+
+def test_password():
+    """Test function for the password
+    """
+    # Testing the case with less than 8 characters
+    assert register.register('username','toto@gmail.com','1234@az') is False
+    # TEsting the case with missing digits
+    assert register.register('username','toto@gmail.com','abcd@azd') is False
+    # Testing the case with missing characters
+    assert register.register('username','toto@gmail.com','1234@123') is False
+    # TEsting the case with missing special character
+    assert register.register('username','toto@gmail.com','12341123') is True
+
+#### Using Monkeypatch
+
+def test_email_with_user_input_no_at_sign(monkeypatch):
+    """Test if an email is missing @
+
+    Args:
+        monkeypatch (_type_): _description_
+    """
+    monkeypatch.setattr('sys.stdin', io.StringIO('petra.adaltas.com'))
+    assert register.get_email_from_input() is None
+
+def test_email_with_user_input_no_dot(monkeypatch):
+    """Test if email has missing .
+
+    Args:
+        monkeypatch (_type_): _description_
+    """
+    monkeypatch.setattr('sys.stdin', io.StringIO('petra@adalatascom'))
+    assert register.get_email_from_input() is None
+
+def test_email_with_user_input_correct(monkeypatch):
+    """Test if email is correct
+
+    Args:
+        monkeypatch (_type_): _description_
+    """
+    monkeypatch.setattr('sys.stdin',io.StringIO('petra@adaltas.com'))
+    assert register.get_email_from_input() == 'petra@adaltas.com'
+
+def test_empty_username(monkeypatch):
+    """Test if an username is missing empty
+
+    Args:
+        monkeypatch (_type_): _description_
+    """
+    monkeypatch.setattr('sys.stdin', io.StringIO(" "))
+    assert register.get_username_from_input() is None
+
+def test_correct_username(monkeypatch):
+    """Test if a username is correct
+
+    Args:
+        monkeypatch (_type_): _description_
+    """
+    monkeypatch.setattr('sys.stdin', io.StringIO("azert12344"))
+    assert register.get_username_from_input() == "azert12344"
+
+def test_short_password(monkeypatch):
+    """Test if the pasword is too short
+
+    Args:
+        monkeypatch (_type_): _description_
+    """
+    monkeypatch.setattr('sys.stdin', io.StringIO("1234@aq"))
+    assert register.get_password_from_input() is None
+
+def test_password_without_special_character(monkeypatch):
+    """Test if the pasword has not a special character
+
+    Args:
+        monkeypatch (_type_): _description_
+    """
+    monkeypatch.setattr('sys.stdin', io.StringIO("12341aqa"))
+    assert register.get_password_from_input() is None
+
+def test_password_without_character(monkeypatch):
+    """Test if the pasword has no character
+
+    Args:
+        monkeypatch (_type_): _description_
+    """
+    monkeypatch.setattr('sys.stdin', io.StringIO("12341@12"))
+    assert register.get_password_from_input() is None
+
+
+def test_password_without_digits(monkeypatch):
+    """Test if the pasword has no digits
+
+    Args:
+        monkeypatch (_type_): _description_
+    """
+    monkeypatch.setattr('sys.stdin', io.StringIO("azert@aq"))
+    assert register.get_password_from_input() is None
+
+def test_correct_password(monkeypatch):
+    """Test if the pasword is correct
+
+    Args:
+        monkeypatch (_type_): _description_
+    """
+    monkeypatch.setattr('sys.stdin', io.StringIO("12345@aq"))
+    assert register.get_password_from_input() == "12345@aq"
+
+def test_wrong_password(monkeypatch):
+    """Test a wrong password
+
+    Args:
+        monkeypatch (_type_): _description_
+    """
+    monkeypatch.setattr('sys.stdin', io.StringIO("azert@aq"))
+    assert register.get_password_from_input() is None
+
+def test_wrong_password1(monkeypatch):
+    """Test a wrong password
+
+    Args:
+        monkeypatch (_type_): _description_
+    """
+    monkeypatch.setattr('sys.stdin', io.StringIO("azert@aq"))
+    assert register.get_password_from_input() is None
+
+def test_wrong_password2(monkeypatch):
+    """Test a wrong password
+
+    Args:
+        monkeypatch (_type_): _description_
+    """
+    monkeypatch.setattr('sys.stdin', io.StringIO("azert@aq"))
+    assert register.get_password_from_input() is None
